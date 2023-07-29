459522854e408f1de780383556bc6bc57c7e8c3d
Sacarima <caroline12alex@yahoo.com>
2023-07-29 22:16:20 +0200

    utilties classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import NavBar from "./scenes/Navbar";
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery"
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home") // this will determine what part of the navigation on the wesite we are yet!
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <div className="App">
-      
+    <div className="app bg-deep-blue">
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
