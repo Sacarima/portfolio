ee3aaa6c56e1d6a52ac3a4ffa00e4da5ce347d37
Sacarima <caroline12alex@yahoo.com>
2023-08-20 02:39:02 +0200

    contact form and form validation

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -107,8 +107,120 @@
 
 // export default Projects;
 
-import LineGradient from "../components/LineGradient";
-import { motion } from "framer-motion";
+// import LineGradient from "../components/LineGradient";
+// import { motion } from "framer-motion";
+
+// const container = {
+//   hidden: {},
+//   visible: {
+//     transition: {
+//       staggerChildren: 0.2,
+//     },
+//   },
+// };
+
+// const projectVariant = {
+//   hidden: { opacity: 0, scale: 0.8 },
+//   visible: { opacity: 1, scale: 1 },
+// };
+
+// const Project = ({ title }) => {
+//     const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 hover:border-dotted transition duration-500
+//         bg-gray z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+//   const projectTitle = title.split(" ").join("-").toLowerCase();
+
+//   return (
+//     <motion.div variants={projectVariant} className="relative">
+//       <div className={overlayStyles}>
+//         <p className="text-2xl font-playfair">{title}</p>
+//         <p className="mt-7">
+//           Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+//           porttitor accumsan tincidunt.
+//         </p>
+//       </div>
+//       <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+//     </motion.div>
+//   );
+// };
+
+// const Projects = () => {
+//   return (
+//     <section id="projects" className="pt-48 pb-48">
+//       {/* HEADINGS */}
+//       <motion.div
+//         className="md:w-2/5 mx-auto text-center"
+//         initial="hidden"
+//         whileInView="visible"
+//         viewport={{ once: true, amount: 0.5 }}
+//         transition={{ duration: 0.5 }}
+//         variants={{
+//           hidden: { opacity: 0, y: -50 },
+//           visible: { opacity: 1, y: 0 },
+//         }}
+//       >
+//         <div>
+//           <p className="font-playfair font-semibold text-4xl">
+//             <span className="text-red">PRO</span>JECTS
+//           </p>
+//           <div className="flex justify-center mt-5">
+//             <LineGradient width="w-2/3" />
+//           </div>
+//         </div>
+//         <p className="mt-10 mb-10">
+//           Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+//           fames odio in at. At magna ornare dictum lectus. Purus massa morbi
+//           purus nec eget eleifend ut elit.
+//         </p>
+//       </motion.div>
+
+//       {/* PROJECTS */}
+//       <div className="flex justify-center">
+//         <motion.div
+//           className="sm:grid sm:grid-cols-3"
+//           variants={container}
+//           initial="hidden"
+//           whileInView="visible"
+//           viewport={{ once: true, amount: 0.2 }}
+//         >
+//           {/* ROW 1 */}
+//           <div
+//             className="flex justify-center text-center items-center p-10 bg-red
+//               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+//           >
+//             BEAUTIFUL USER INTERFACES
+//           </div>
+//           <Project title="Project 1" />
+//           <Project title="Project 2" />
+
+//           {/* ROW 2 */}
+//           <Project title="Project 3" />
+//           <Project title="Project 4" />
+//           <Project title="Project 5" />
+
+//           {/* ROW 3 */}
+//           <Project title="Project 6" />
+//           <Project title="Project 7" />
+//           <div
+//             className="flex justify-center text-center items-center p-10 bg-blue
+//               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+//           >
+//             SMOOTH USER EXPERIENCE
+//           </div>
+//         </motion.div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Projects;
+
+/********************** */
+
+
+
+
+import LineGradient from '../components/LineGradient';
+import { motion } from 'framer-motion';
 
 const container = {
   hidden: {},
@@ -124,26 +236,35 @@ const projectVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
-const Project = ({ title }) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-    bg-grey z-60 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
-  const projectTitle = title.split(" ").join("-").toLowerCase();
+const Project = ({ title, imageNumber }) => {
+  const overlayStyles = 'absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-gray-300 z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue';
+  const imageUrl = `../assets/project-${imageNumber}.jpeg`;
 
   return (
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
         <p className="mt-7">
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-          porttitor accumsan tincidunt.
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+          Nulla porttitor accumsan tincidunt.
         </p>
       </div>
-      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+      <img src={imageUrl} alt={title} />
     </motion.div>
   );
 };
 
 const Projects = () => {
+  const projects = [
+    { title: 'Project 1', imageNumber: 1 },
+    { title: 'Project 2', imageNumber: 2 },
+    { title: 'Project 3', imageNumber: 3 },
+    { title: 'Project 4', imageNumber: 4 },
+    { title: 'Project 5', imageNumber: 5 },
+    { title: 'Project 6', imageNumber: 6 },
+    { title: 'Project 7', imageNumber: 7 },
+  ];
+
   return (
     <section id="projects" className="pt-48 pb-48">
       {/* HEADINGS */}
@@ -189,17 +310,11 @@ const Projects = () => {
           >
             BEAUTIFUL USER INTERFACES
           </div>
-          <Project title="Project 1" />
-          <Project title="Project 2" />
-
-          {/* ROW 2 */}
-          <Project title="Project 3" />
-          <Project title="Project 4" />
-          <Project title="Project 5" />
+          {projects.map((project, index) => (
+            <Project key={index} title={project.title} imageNumber={project.imageNumber} />
+          ))}
 
           {/* ROW 3 */}
-          <Project title="Project 6" />
-          <Project title="Project 7" />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
